94c1965ea3aba4648d0d1610b8ad4ff606e9a260
Unknown <rem.coulombe@gmail.com>
2018-01-25 21:40:53 -0500

    creation du reset.css dans les fichiers

---
+++ php/php/blocks/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <ul>
+        <li><a href="#">rem.coulombe@gmail.com</a></li>
+        <li><a href="#">(514) 572-6773</a></li>
+    </ul>
+
+    <ul>
+        <li><img src="https://soundcloud.com/coulombe36"/></li>
+        <li><img src="https://vimeo.com/user44672269"/></li>
+    </ul>
+</footer>
--- php/php/index.php
+++ php/php/index.php
@@ -1,5 +1,10 @@
-<?php
-
-echo "a bin criss";
-
-?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+</head>
+<body>
+    <?php include("blocks/header.php") ?>
+    <main></main>
+    <?php include("blocks/footer.php") ?>
+</body>
+</html>
