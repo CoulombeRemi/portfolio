db269e7d803fd43f423a0f5c7dcb1f796918b2cf
Unknown <rem.coulombe@gmail.com>
2018-01-25 21:48:20 -0500

    Creation de style.css

--- php/php/index.php
+++ php/php/index.php
@@ -1,6 +1,11 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-    <title></title>
+    <title>Rémi Coulombe</title>
+
+    <meta charset="utf-8" />
+
+    <link rel="stylesheet" type="text/css" href="../style/reset.css" />
+    <link rel="stylesheet" type="text/css" href="../style/style.css" />
 </head>
 <body>
     <?php include("blocks/header.php") ?>
