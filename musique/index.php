dd5b413e38668f3c04bf1e9605abf82973fc62bd
Rémi Coulombe <rem.coulombe@gmail.com>
2018-02-06 13:45:03 -0500

    css iframe

--- musique/index.php
+++ musique/index.php
@@ -30,6 +30,9 @@
                     <li>
                         <iframe scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/390616188&amp;color=%2300ff9d&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>   
                     </li>
+                    <li>
+                        <iframe scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/390616188&amp;color=%2300ff9d&amp;auto_play=false&amp;hide_related=false&amp;show_comments=false&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe>   
+                    </li>
                 </ul>
             </div>
                 
