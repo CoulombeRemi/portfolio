921f2aba54fc8bf61a1b25da2ed6017a47902e32
Rémi Coulombe <rem.coulombe@gmail.com>
2018-02-19 16:49:39 -0500

    creation page a propos

--- blocks/header.php
+++ blocks/header.php
@@ -1,10 +1,10 @@
 <header>
 <div  class="container">
-        <a href="#" class="logo">Rémi Coulombe</a>
+        <a href="/" class="logo">Rémi Coulombe</a>
         <ul class="menu">
             <li><a href="/musique">audio</a></li>
             <li><a href="#">autres projets</a></li>
-            <li><a href="#">à propos</a></li>
+            <li><a href="/propos">à propos</a></li>
             <li><a href="/contact">contact</a></li>
         </ul>
 
---
+++ propos/index.php
@@ -0,0 +1,20 @@
+<?php 
+if(file_exists("../settings.php")){include("../settings.php");}
+?>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Propos</title>
+
+    <?php include("../blocks/head.php") ?>
+</head>
+<body class="propos">
+    <?php include("../blocks/header.php") ?>
+    <main>
+        <div class="container">
+            <h1>À Propos</h1>
+        </div>
+    </main>
+    <?php include("../blocks/footer.php") ?>
+</body>
+</html>
