3ce3787cee8457c006ebab4c120e87992ebe6e42
CoulombeRemi <rem.coulombe@gmail.com>
2018-03-20 09:29:49 -0400

    css affiche - web

--- projets/index.php
+++ projets/index.php
@@ -13,37 +13,10 @@ if(file_exists("../settings.php")){include("../settings.php");}
     <main>
         <div class="container">
             <h1>Autres Projets</h1>
-            <div class="musique-frame">
-                <ul class="ul-musique">
-                    <li class="affiche">
-                        <div class="aff">
-                            <img src="../assets/img/projet/affiche_primaire_st-valentin_2018.jpg" alt="p_st-v_2018">
-                        </div>
-                    </li>
-                    <li class="affiche">
-                        <div class="aff">
-                            <img src="../assets/img/projet/affiche_primaire_st-valentin_2017.jpg" alt="p_st-v_2017">
-                        </div>
-                    </li>
-                    <li class="affiche">
-                        <div class="aff">
-                            <img src="../assets/img/projet/affiche_seondaire_st-valentin_2016_final.jpg" alt="s_st-v_2016">
-                        </div>
-                    </li>
-                    <li class="affiche">
-                        <div class="aff">
-                            <img src="../assets/img/projet/affiche_primaire_halloween_2016.jpg" alt="p_H_2016">
-                        </div>
-                    </li>
-                    <li class="affiche">
-                        <div class="aff">
-                            <img src="../assets/img/projet/affiche_secondaire_2015.jpg" alt="s_2016">
-                        </div>
-                    </li>
-                </ul>
-            </div>
 
-            <!-- <ul class="ul-musique"> -->
+            
+
+            <!-- video -->
                 <h2>Vidéo</h2>
                 <div class="musique-frame">
                     <ul class="ul-musique" id="ul-video">
@@ -84,16 +57,54 @@ if(file_exists("../settings.php")){include("../settings.php");}
                             </div>  
                         </li>
                     </ul>
-
                 </div>
-            <!-- </ul> -->
-            <ul class="ul-musique">
-                <h2>Web</h2>
-                <li class="site-web">
-                    <p>Site web Techniques Intégration Multimédia 2017</p>
-                    <a href="http://tim.cmaisonneuve.qc.ca"><img src="../assets/img/projet/timMaisonneuve.jpg" alt="site tim"></a>
-                </li>
-            </ul>
+
+            <!-- web -->
+            <h2>Web</h2>
+            <div class="musique-frame">
+                <ul class="ul-musique" id="ul-web">
+                    <li class="site-web">
+                        <div><a href="http://tim.cmaisonneuve.qc.ca"><img src="../assets/img/projet/timMaisonneuve.jpg" alt="site tim"></a></div>
+                        <div>
+                            <h3>TIM 2017</h3>
+                            <p>Site web gagnant comme nouveau site du département de Techniques d'Intégration Multimédia 2017. Fait en Word Press</p>
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <!-- infography -->
+            <h2>Infographie</h2>
+            <div class="musique-frame">
+                <ul class="ul-musique" id="ul-aff">
+                    <li class="affiche">
+                        <div class="aff">
+                            <img src="../assets/img/projet/affiche_primaire_st-valentin_2018.jpg" alt="p_st-v_2018">
+                        </div>
+                    </li>
+                    <li class="affiche">
+                        <div class="aff">
+                            <img src="../assets/img/projet/affiche_primaire_st-valentin_2017.jpg" alt="p_st-v_2017">
+                        </div>
+                    </li>
+                    <li class="affiche">
+                        <div class="aff">
+                            <img src="../assets/img/projet/affiche_seondaire_st-valentin_2016_final.jpg" alt="s_st-v_2016">
+                        </div>
+                    </li>
+                    <li class="affiche">
+                        <div class="aff">
+                            <img src="../assets/img/projet/affiche_primaire_halloween_2016.jpg" alt="p_H_2016">
+                        </div>
+                    </li>
+                    <li class="affiche">
+                        <div class="aff">
+                            <img src="../assets/img/projet/affiche_secondaire_2015.jpg" alt="s_2016">
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
         </div>
     </main>
     <?php include("../blocks/footer.php") ?>
