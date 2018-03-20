aab7de4b65e1f563cb7e326ff0b9fe2736864f3a
CoulombeRemi <rem.coulombe@gmail.com>
2018-03-20 11:08:02 -0400

    magnific popup

--- propos/index.php
+++ propos/index.php
@@ -13,6 +13,11 @@ if(file_exists("../settings.php")){include("../settings.php");}
     <main>
         <div class="container">
             <h1>À Propos</h1>
+            <p class="pro-text">Présentement, je suis en train de terminer mon DEC en techniques d’intégration multimédia au collège de Maisonneuve. Je souhaite poursuivre mes études universitaires dans le domaine musical à votre université.</p>
+            <p class="pro-text">Intéressé par la musique depuis toujours, j’ai débuté en jouant de la guitare durant plusieurs années. Au secondaire, j’ai découvert la musique par ordinateur tels que l’enregistrement et la composition. Étant curieux sur le domaine, j’ai développé mon côté autodidacte en apprenant grandement par moi-même l’utilisation des logiciels audios et les bases de la théorie musicale. 
+            </p>
+            <p class="pro-text">Ma formation collégiale m’a permis de toucher plusieurs aspects du multimédia. Même si j’ai implanté certaines de mes connaissances musicales dans certains de mes cours du DEC, je n’ai pas pu approfondir cette discipline au maximum.Par le programme que vous offrez, musique numérique, intégrer ce parcours me permettrait d’élever mes notions musicales et de transformer ma passion de niveau «hobby» vers un niveau professionnel. De part mes compétences, je crois avoir le profil nécessaire pour intégrer le programme.
+            </p>
         </div>
     </main>
     <?php include("../blocks/footer.php") ?>
