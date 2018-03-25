eed70108dbf9a1f67c10ee1a0f04000d5297ebee
Rémi Coulombe <rem.coulombe@gmail.com>
2018-03-25 15:04:41 -0400

    ghf

--- contact/index.php
+++ contact/index.php
@@ -22,7 +22,7 @@ if(file_exists("../settings.php")){include("../settings.php");}
                     </li>
                     <li>
                         <img src="../assets/img/phone.svg" /> 
-                            <p>514.572.6773</p> 
+                            <p>(514) 572-6773</p> 
                     </li>
                     <li>
                         <img src="../assets/img/map.svg" /> 
@@ -35,19 +35,19 @@ if(file_exists("../settings.php")){include("../settings.php");}
                 </ul>
                 <ul class="info-exSite">
                     <li>
-                        <a href="https://soundcloud.com/coulombe36">
+                        <a href="https://soundcloud.com/coulombe36" target="_blank">
                             <img src="../assets/img/soundcloud.svg" />    
                             <!-- <p>Soundcloud</p> -->
                         </a>  
                     </li>
                     <li>
-                        <a href="https://vimeo.com/user44672269">
+                        <a href="https://vimeo.com/user44672269" target="_blank">
                             <img src="../assets/img/vimeo.svg" />    
                             <!-- <p>Vimeo</p> -->
                         </a>
                     </li>
                     <li>
-                        <a href="https://www.behance.net/coulombe">
+                        <a href="https://www.behance.net/coulombe" target="_blank">
                             <img src="../assets/img/behance.svg" />    
                             <!-- <p>Behance</p> -->
                         </a>
