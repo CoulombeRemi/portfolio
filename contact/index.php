3081eb319a027b6addebd4a414bee0ac777a6183
Rémi Coulombe <rem.coulombe@gmail.com>
2018-03-25 13:08:40 -0400

    css contact 2.0

--- contact/index.php
+++ contact/index.php
@@ -14,41 +14,50 @@ if(file_exists("../settings.php")){include("../settings.php");}
             <h1>Contact</h1>
             <div class="info-contact">
                 <ul class="info-liste">
-                    <li><p>Rémi Coulombe</p></li>
+                    <li><h2>Rémi Coulombe</h2></li>
                     <li>
                         <img src="../assets/img/mail.svg" />
-                        <p>rem.coulombe@gmail.com</p>
+                        <div>
+                            <p>rem.coulombe@gmail.com</p>
+                        </div>
+                        
                     </li>
                     <li>
                         <img src="../assets/img/phone.svg" /> 
-                        <p>514.572.6773</p>
+                        <div>
+                            <p>514.572.6773</p>
+                        </div> 
                     </li>
                     <li>
-                        <img src="../assets/img/map.svg" />   
-                        <p>Montréal, Qc</p>
+                        <img src="../assets/img/map.svg" /> 
+                        <div>
+                            <p>Montréal, Qc</p>  
+                        </div>   
                     </li>
                     <li>
                         <img src="../assets/img/map.svg" />    
-                        <p>Granby, Qc</p>
+                        <div>
+                            <p>Granby, Qc</p>
+                        </div>
                     </li>
                 </ul>
                 <ul class="info-exSite">
                     <li>
                         <a href="https://soundcloud.com/coulombe36">
                             <img src="../assets/img/soundcloud.svg" />    
-                            <p>Soundcloud</p>
+                            <!-- <p>Soundcloud</p> -->
                         </a>  
                     </li>
                     <li>
                         <a href="https://vimeo.com/user44672269">
                             <img src="../assets/img/vimeo.svg" />    
-                            <p>Vimeo</p>
+                            <!-- <p>Vimeo</p> -->
                         </a>
                     </li>
                     <li>
                         <a href="https://www.behance.net/coulombe">
                             <img src="../assets/img/behance.svg" />    
-                            <p>Behance</p>
+                            <!-- <p>Behance</p> -->
                         </a>
                     </li>
                     <li>
