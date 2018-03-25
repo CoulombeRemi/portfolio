b7fb8a73a634549b4d5f26d081c14f8a145c2817
Rémi Coulombe <rem.coulombe@gmail.com>
2018-03-25 11:11:01 -0400

    delete pop hover web

--- projets/index.php
+++ projets/index.php
@@ -109,9 +109,6 @@ if(file_exists("../settings.php")){include("../settings.php");}
                     <ul class="ul-musique" id="ul-web">
                         <li class="site-web">
                             <div><a href="http://tim.cmaisonneuve.qc.ca"><img src="../assets/img/projet/timMaisonneuve.jpg" alt="site tim"></a></div>
-                            <div class="middle">
-                                <div class="text">John Doe</div>
-                            </div>
                             <div>
                                 <h3>TIM 2017</h3>
                                 <p>Site web gagnant comme nouveau site du département de Techniques d'Intégration Multimédia 2017. Fait en Word Press</p>
